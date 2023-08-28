6a3902ec5fc9d13a609fb56aead748f13018ce84
GreeFox <122176706+GreeFoxTheDev@users.noreply.github.com>
2023-08-28 22:44:56 +0200

    Add files via upload

---
+++ Katanas/src/main/java/me/greefox/greefox/me/Greefox/Katanas.java
@@ -0,0 +1,40 @@
+package me.greefox.greefox.me.Greefox;
+
+import me.greefox.greefox.me.Greefox.Commands.Give;
+import me.greefox.greefox.me.Greefox.KatanasTypes.Copper;
+import me.greefox.greefox.me.Greefox.KatanasTypes.Diamond;
+import me.greefox.greefox.me.Greefox.KatanasTypes.Gold;
+import me.greefox.greefox.me.Greefox.KatanasTypes.Iron;
+import me.greefox.greefox.me.Greefox.Listeners.KatanaListener;
+import me.greefox.greefox.me.Greefox.Listeners.ResourcepackChecker;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Katanas extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
+        console.sendMessage(ChatColor.AQUA + "******************************");
+        console.sendMessage(ChatColor.AQUA + "| Katanas have been enabled! |");
+        console.sendMessage(ChatColor.AQUA + "******************************");
+        this.getCommand("givekatana").setExecutor(new Give(this));
+        this.getCommand("givekatana").setTabCompleter(new Give(this));
+        Diamond.init();
+        Gold.init();
+        Iron.init();
+        Copper.init();
+        getServer().getPluginManager().registerEvents(new KatanaListener(this), this);
+        getServer().getPluginManager().registerEvents(new ResourcepackChecker(this), this);
+    }
+
+    @Override
+    public void onDisable() {
+        ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
+        console.sendMessage(ChatColor.RED + "*******************************");
+        console.sendMessage(ChatColor.RED + "| Katanas have been disabled! |");
+        console.sendMessage(ChatColor.RED + "*******************************");
+    }
+}
